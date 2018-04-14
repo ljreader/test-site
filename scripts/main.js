adc67cb9774f1541f1acc517927fd0a21b76c521
ljreader <l.j.reader@hotmail.com>
2018-04-14 14:09:30 -0600

    initial

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+	function b() {
+		var a = document.createElement("bootstrap"),
+			b = {
+				WebkitTransition: "webkitTransitionEnd",
+				MozTransition: "transitionend",
+				OTransition: "oTransitionEnd otransitionend",
+				transition: "transitionend"
+			};
+		for (var c in b)
+			if (void 0 !== a.style[c]) return {
+				end: b[c]
+			};
+		return !1;
+	}
